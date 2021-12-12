8059d7f479cf0b1dc15e97e55dcecbcaba5847e4
Ilya Rogozin <rogozin.il2399@gmail.com>
2021-12-12 15:37:28 +0400

    one more commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
-from rest_framework import filters
-from rest_framework import mixins
+from rest_framework import mixins, filters, viewsets
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
                                         IsAuthenticated)
 from rest_framework.pagination import LimitOffsetPagination
